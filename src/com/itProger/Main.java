ad6455f091b83fdcbcaf1b66318895680cc4b8c7
AndreevIP <Andreev2102@inbox.ru>
2019-08-16 11:38:06 +0300

    Задача 9

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размер массива и нажмите Enter: ");
+
         int x = scanner.nextInt();
         int array[] = new int[x];
-       for(int i = 0; i< array.length; i++)
-       {
-           System.out.println("Введите число и нажмите Enter: ");
-           array[i] = scanner.nextInt();
-       }
-            for (int element: array)
-            {
-                System.out.println("Вывод элемента массива, умноженного на 2:");
-                System.out.println(element*2);
-            }
 
+        for(int i = 0; i< array.length; i++)
+        {
+            System.out.println("Введите " + (i+1) + "-е число и нажмите Enter: ");
+            array[i] = scanner.nextInt();
+        }
+
+        int i=1;
+
+        for (int element: array)
+        {
+            System.out.println("Вывод " + (i++) + "-го числа, умноженного на 2:");
+            System.out.println(element*2);
+        }
     }
 }
